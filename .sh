601abc982c850f3d55161ece0029fd1f14bc14aa
JactusTheCactus <threateninglyamusing@gmail.com>
2026-01-03 17:39:41 -0800

    Update .sh

--- .sh
+++ .sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 compile() {
+	local out="logs/out/$1.log"
+	local err="logs/err/$1.log"
 	g++ \
 		"src/$1.cpp" \
 		-std=c++20 \
 		-lncurses \
 		-o "bin/$1" \
-		> "logs/out/$1.log" \
-		2> "logs/err/$1.log"
+		> "$out" \
+		2> "$err" \
+		|| code "$err"
 }
 rm -r bin logs 2> /dev/null || :
 mkdir bin logs logs/out logs/err
